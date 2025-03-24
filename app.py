c1c354e3688a3164c4eab1ca5b8f3ff56433e291
Nguyễn Tuấn Dũng <97139064+dungnthe150915FPT@users.noreply.github.com>
2025-03-24 15:21:29 +0700

    update anh gia dinh

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Trang chủ
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Trang sự kiện
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
